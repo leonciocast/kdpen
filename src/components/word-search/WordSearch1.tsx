3b7be4419cf8c55435296faecbb7a90daefc1776
saadicloud <muhammadsaadm95@gmail.com>
2024-01-26 20:52:31 +0500

    add multiple functionality in word search game

--- src/components/word-search/WordSearch1.tsx
+++ src/components/word-search/WordSearch1.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
 import CSVReader from "react-csv-reader";
@@ -8,26 +8,36 @@ interface Orientation {
   dx: number;
   dy: number;
 }
-// interface CellType {
-//     letter : string,
-//     isRandom : boolean
-// }
+
 const WordSearch1: React.FC = () => {
-  const [board, setBoard] = useState<string[][]>([]);
+  const [boards, setBoards] = useState<string[][][]>([]);
   const [inputWord, setInputWord] = useState<string>("");
   const [wordArray, setWordArray] = useState<string[]>([
-    
-    "PYTHON",
-    "HTML5",
-    
+    // "PYTHON",
+    // "HTML5",
   ]);
+
+  const dividedArray : string[][] = useMemo(()=>{
+    const wordsArrays: string[][] = [];
+    for (let i = 0; i < wordArray.length; i += 10) {
+      wordsArrays.push(wordArray.slice(i, i + 10));
+    }
+    return wordsArrays
+  }, [wordArray])
+
+  console.log("Divided arrays are: ",dividedArray)
+  console.log("Our final Boards are: ", boards)
+
+
   const [horizontal, setHorizontal] = useState<boolean>(true);
   const [vertical, setVertical] = useState<boolean>(true);
   const [diagonalTopLeft, setDiagonalTopLeft] = useState<boolean>(true);
   const [diagonalBottomLeft, setDiagonalBottomLeft] = useState<boolean>(true);
+
+
   const [showSolution, setShowSolution] = useState<boolean>(false);
 
-  console.log(board);
+  // console.log(board);
   const getRandomInt = (max: number): number => {
     return Math.floor(Math.random() * Math.floor(max));
   };
@@ -100,62 +110,68 @@ const WordSearch1: React.FC = () => {
     return board;
   };
 
-  const generatePuzzle = (): void => {
-    let newBoard = Array.from({ length: 13 }, () =>
-      Array(13).fill("-")
-    ) as string[][];
 
-    const selectedDirections: Orientation[] = [];
-
-    if (horizontal) selectedDirections.push({ dx: 1, dy: 0 });
-    if (vertical) selectedDirections.push({ dx: 0, dy: 1 });
-    if (diagonalTopLeft) selectedDirections.push({ dx: 1, dy: 1 });
-    if (diagonalBottomLeft) selectedDirections.push({ dx: 1, dy: -1 });
-
-    for (const word of wordArray) {
-      newBoard = placeWord(newBoard, word, selectedDirections);
-    }
-
-    newBoard = fillEmpty(newBoard);
-    setBoard(newBoard);
-  };
 
   const handleAddWord = (): void => {
-    if (inputWord.trim() !== "" && wordArray.length <= 9) {
-      setWordArray([...wordArray, inputWord.toUpperCase()]);
+    if (inputWord.trim() !== "") {
+      setWordArray((prevWordArray) => [...prevWordArray, inputWord.toUpperCase()]);
       setInputWord("");
     } else {
-      alert("You have reached the maximum number of words!");
+      alert("Enter Appropriate word");
     }
   };
+  
 
   const downloadPDF = () => {
     const componentRef = document.getElementById(`wordsearch`) as HTMLElement;
 
-    html2canvas(componentRef).then(canvas => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF ({
-            orientation: 'portrait',
-            unit: 'mm',
-            format: 'a4',
+    html2canvas(componentRef).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF({
+        orientation: "portrait",
+        unit: "mm",
+        format: "a4",
+      });
 
-        });
+      const imgWidth = 150;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
 
-        const imgWidth = 150;
-        const imgHeight = (canvas.height * imgWidth) / canvas.width;
-        // pdf.text(`crossword`,0,0)
-
-        pdf.addImage(imgData, 'PNG', 30,10, imgWidth, imgHeight);
-        pdf.save(`wordsearch.pdf`);
+      pdf.addImage(imgData, "PNG", 30, 10, imgWidth, imgHeight);
+      pdf.save(`wordsearch.pdf`);
     });
-};
-const handleFileUpload = (data: string[][], fileInfo: any): void => {
-  const wordsFromFile = data.map((item) => item[0]);
-  setWordArray([...wordArray, ...wordsFromFile]);
-  console.log("test", wordsFromFile)
-};
+  };
 
-  // Assuming board is an HTML element with the id "board"
+  const generatePuzzleForWords = (wordsArray: string[]): string[][] => {
+    let newBoard = Array.from({ length: 13 }, () => Array(13).fill("-")) as string[][];
+  
+    const selectedDirections: Orientation[] = [];
+    if (horizontal) selectedDirections.push({ dx: 1, dy: 0 });
+    if (vertical) selectedDirections.push({ dx: 0, dy: 1 });
+    if (diagonalTopLeft) selectedDirections.push({ dx: 1, dy: 1 });
+    if (diagonalBottomLeft) selectedDirections.push({ dx: 1, dy: -1 });
+  
+    for (const word of wordsArray) {
+      newBoard = placeWord(newBoard, word, selectedDirections);
+    }
+  
+    // setBoards(dividedArray);
+    return fillEmpty(newBoard);
+  };
+
+  
+  // useEffect(() => {
+  //   setBoards(dividedArray);
+  // }, [dividedArray]);
+  
+  const handleFileUpload = (data: string[][], fileInfo: any): void => {
+    // Extract words from CSV data
+    const wordsFromFile = data.map((item) => item[0]);
+    setWordArray([...wordArray, ...wordsFromFile]);
+
+  };
+
+ 
+
   return (
     <div className="container mt-5">
       <div className="mb-3">
@@ -170,9 +186,8 @@ const handleFileUpload = (data: string[][], fileInfo: any): void => {
           onChange={(e) => setInputWord(e.target.value)}
         />
       </div>
-
-      <label className="btn btn-info me-2">
-        Upload CSV
+      <div className="d-flex align-items-center mb-3">
+        <label className=" fw-bold me-2">Upload CSV</label>
         <CSVReader
           onFileLoaded={handleFileUpload}
           parserOptions={{
@@ -181,7 +196,7 @@ const handleFileUpload = (data: string[][], fileInfo: any): void => {
             skipEmptyLines: true,
           }}
         />
-      </label>
+      </div>
 
       <div className="mb-3">
         <label className="form-check-label me-2">
@@ -228,152 +243,42 @@ const handleFileUpload = (data: string[][], fileInfo: any): void => {
       <button className="btn btn-primary me-2" onClick={handleAddWord}>
         Add Word
       </button>
-      
 
-      <button className="btn btn-success" onClick={generatePuzzle}>
-        Generate Puzzle
+      <button className="btn btn-success"  onClick={() =>{
+        setBoards([])
+        dividedArray.map((single_array, i)=>{
+          setBoards(prevBoards=>{
+            return [
+              ...prevBoards,
+              generatePuzzleForWords(single_array)
+            ]
+          })
+        })
+      }}>
+        Generate Puzzles
       </button>
 
       <button className="btn btn-primary mx-1" onClick={() => downloadPDF()}>
         Download
       </button>
 
-      <button className="btn btn-dark" onClick={() => setShowSolution(!showSolution)}>{showSolution ? `Hide`:`Show`} Solution</button>
+      <button
+        className="btn btn-dark"
+        onClick={() => setShowSolution(!showSolution)}
+      >
+        {showSolution ? `Hide` : `Show`} Solution
+      </button>
 
-      
-        <div className="row w-75">
-          <div className="col-md-3 position-relative">
-            <button
-              className="carousel-control-prev"
-              onClick={generatePuzzle}
-              type="button"
-              data-bs-target="#carouselExample"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon bg-black z-1"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-          </div>
-          
-          <div id={`wordsearch`} className="col-md-6">
-            <div id="carouselExample" className="carousel slide">
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                    
-                  <table className="table text-center w-100 table-bordered mt-3">
-                    <tbody>
-                      {board.map((row, rowIndex) => (
-                        <tr key={rowIndex}>
-                          {row.map((cell, colIndex) => {
-                            let parsedCell = JSON.parse(cell);
-
-                            return (
-                              <td
-                                className="p-1"
-                                key={colIndex}
-                                style={{
-                                  width: "25px",
-                                  height: "25px",
-                                //   background: parsedCell?.orignalLetter && showSolution
-                                //     ? "gray"
-                                //     : "white",
-                                }}
-                              >
-                                <span  style={{
-                                 
-                                  background: (parsedCell?.orignalLetter && showSolution)&& "antiquewhite",
-                                }}>
-                                {parsedCell?.orignalLetter
-                                  ? parsedCell?.orignalLetter
-                                  : parsedCell?.randomLetter}
-                                    </span>
-                              </td>
-                            );
-                          })}
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-                <div className="carousel-item">
-                <table className="table text-center w-100 table-bordered mt-3">
-                    <tbody>
-                      {board.map((row, rowIndex) => (
-                        <tr key={rowIndex}>
-                          {row.map((cell, colIndex) => {
-                            let parsedCell = JSON.parse(cell);
-
-                            return (
-                              <td
-                                className="p-1"
-                                key={colIndex}
-                                style={{
-                                  width: "25px",
-                                  height: "25px",
-                                //   background: parsedCell?.orignalLetter && showSolution
-                                //     ? "gray"
-                                //     : "white",
-                                }}
-                              >
-                                <span  style={{
-                                 
-                                  background: (parsedCell?.orignalLetter && showSolution)&& "antiquewhite",
-                                }}>
-                                {parsedCell?.orignalLetter
-                                  ? parsedCell?.orignalLetter
-                                  : parsedCell?.randomLetter}
-                                    </span>
-                              </td>
-                            );
-                          })}
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-                <div className="carousel-item">
-                <table className="table text-center w-100 table-bordered mt-3">
-                    <tbody>
-                      {board.map((row, rowIndex) => (
-                        <tr key={rowIndex}>
-                          {row.map((cell, colIndex) => {
-                            let parsedCell = JSON.parse(cell);
-
-                            return (
-                              <td
-                                className="p-1"
-                                key={colIndex}
-                                style={{
-                                  width: "25px",
-                                  height: "25px",
-                                //   background: parsedCell?.orignalLetter && showSolution
-                                //     ? "gray"
-                                //     : "white",
-                                }}
-                              >
-                                <span  style={{
-                                 
-                                  background: (parsedCell?.orignalLetter && showSolution)&& "antiquewhite",
-                                }}>
-                                {parsedCell?.orignalLetter
-                                  ? parsedCell?.orignalLetter
-                                  : parsedCell?.randomLetter}
-                                    </span>
-                              </td>
-                            );
-                          })}
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
+      <div className="row w-75">
+        {boards.map((single_board, single_board_index) => {
+          return (
+            <div id={`wordsearch`} className="col-md-6">
+              <SinglePuzzle board={single_board} key={single_board_index} />
             </div>
+          );
+        })}
 
-            <div className="mt-3">
+        <div className="mt-3">
           <h5>Word List:</h5>
           <ul>
             {wordArray.map((word, index) => (
@@ -381,29 +286,55 @@ const handleFileUpload = (data: string[][], fileInfo: any): void => {
             ))}
           </ul>
         </div>
-          </div>
-          
 
-          <div className="col-md-3 position-relative">
-            <button
-              className="carousel-control-next"
-              onClick={generatePuzzle}
-              type="button"
-              data-bs-target="#carouselExample"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon bg-black z-1"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-        
-        
       </div>
     </div>
   );
 };
 
 export default WordSearch1;
+
+interface SinglePuzzleProp {
+  board: string[][];
+}
+const SinglePuzzle: React.FC<SinglePuzzleProp> = ({ board }) => {
+  return (
+    <div>
+      <table className="table text-center w-100 table-bordered mt-3">
+        <tbody>
+          {board.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, colIndex) => {
+                let parsedCell = JSON.parse(cell);
+
+                return (
+                  <td
+                    className="p-1"
+                    key={colIndex}
+                    style={{
+                      width: "25px",
+                      height: "25px",
+                      //   background: parsedCell?.orignalLetter && showSolution
+                      //     ? "gray"
+                      //     : "white",
+                    }}
+                  >
+                    <span
+                      style={{
+                        background: parsedCell?.orignalLetter && "antiquewhite",
+                      }}
+                    >
+                      {parsedCell?.orignalLetter
+                        ? parsedCell?.orignalLetter
+                        : parsedCell?.randomLetter}
+                    </span>
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
